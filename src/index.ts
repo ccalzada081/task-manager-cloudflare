c9ee55eb1842842eaaaf521a0a78a7d2fa7c5b72
CarlosCalzadaIteso <carlos.calzada@iteso.mx>
2026-04-30 19:34:27 -0600

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,30 @@
  */
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!");
-	},
-} satisfies ExportedHandler<Env>;
+  async fetch(request: Request, env: any): Promise<Response> {
+    const url = new URL(request.url);
+
+    // GET /tasks
+    if (request.method === "GET" && url.pathname === "/tasks") {
+      const { results } = await env.task_db
+        .prepare("SELECT * FROM tasks")
+        .all();
+
+      return Response.json(results);
+    }
+
+    // POST /tasks
+    if (request.method === "POST" && url.pathname === "/tasks") {
+      const body = await request.json();
+
+      await env.task_db
+        .prepare("INSERT INTO tasks (title) VALUES (?)")
+        .bind(body.title)
+        .run();
+
+      return Response.json({ message: "Task creada" });
+    }
+
+    return new Response("Not Found", { status: 404 });
+  }
+};
